2699fed21a42c577ec216722ba36619887e654b1
Ruiye Ni <RuiyeNi@users.noreply.github.com>
2019-06-06 23:55:21 -0400

    Add functions

--- JavaScript.js
+++ JavaScript.js
@@ -21,3 +21,25 @@ while (x<21) {
     x=x+1
     
 }
+
+
+
+/*
+ * Functions
+ */
+// function expression catSays
+var catSays = function(max) {
+  var catMessage = "";
+  for (var i = 0; i < max; i++) {
+    catMessage += "meow ";
+  }
+  return catMessage;
+};
+
+// function declaration helloCat accepting a callback
+function helloCat(callbackFunc) {
+  return "Hello " + callbackFunc(3);
+}
+
+// pass in catSays as a callback function
+helloCat(catSays);
