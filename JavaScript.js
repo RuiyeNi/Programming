c39d63191f548081175ff06bef901ca5cc1b4448
Ruiye Ni <RuiyeNi@users.noreply.github.com>
2019-06-09 23:47:53 -0400

    AddEventListener

--- JavaScript.js
+++ JavaScript.js
@@ -70,3 +70,14 @@ test.forEach(function(num){
 });
 
 console.log(test)
+
+
+// addEventListener
+const lotsOfElements = document.querySelectorAll('.quizzing-quizzby');
+const element = lotsOfElements[2];
+
+element.addEventListener('animationend', function () {
+    const mainHeading = document.querySelector('h1');
+
+    mainHeading.style.backgroundColor = 'purple'; 
+});
